ce683458a07e1487a022b4c42c55c0d155f4da74
zwm <wenmin@enhance.cn>
2016-06-27 13:29:09 +0800

    x

--- popMM/ViewController.m
+++ popMM/ViewController.m
@@ -1,6 +1,6 @@
 //
 //  ViewController.m
-//  popMM
+//  popM
 //
 //  Created by zwm on 16/6/23.
 //  Copyright © 2016年 zwm. All rights reserved.
@@ -9,19 +9,125 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    CGFloat _bottomH;
+}
+@property (weak, nonatomic) IBOutlet UIView *backView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topLayout;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *heightLayout;
+@property (weak, nonatomic) IBOutlet UIView *bottomView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _tableView.scrollIndicatorInsets = UIEdgeInsetsMake(44, 0, 0, 0);
+    _bottomH = _bottomView.frame.size.height;
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableViewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"cell";
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    [cell viewWithTag:999].hidden = indexPath.row == 0 ? FALSE : TRUE;
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 44;
+}
+
+- (nullable UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *v = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
+    v.backgroundColor = [UIColor whiteColor];
+    UILabel *l = [[UILabel alloc] initWithFrame:CGRectMake(16, 0, self.view.frame.size.width - 32, 44)];
+    l.text = @"只是头部";
+    [v addSubview:l];
+    return v;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.01f;
+}
+
+#pragma mark - UIScrollViewDelegate
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGFloat offset = scrollView.contentOffset.y + scrollView.frame.size.height - scrollView.contentSize.height;
+    if (offset > 0) {
+        _heightLayout.constant = _bottomH + offset;
+    } else {
+        if (_heightLayout.constant != _bottomH) {
+            if (_heightLayout.constant > _bottomH + 10) {
+                _heightLayout.constant = _bottomH;
+                [_bottomView setNeedsLayout];
+                [UIView animateWithDuration:0.25 animations:^{
+                    [_bottomView layoutIfNeeded];
+                }];
+            } else {
+                _heightLayout.constant = _bottomH;
+            }
+        }
+    }
+    if (scrollView.contentOffset.y <= 0) {
+        scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(44 - scrollView.contentOffset.y, 0, 0, 0);
+    }
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate
+{
+    if (scrollView.contentOffset.y < -100) {
+        [self hideAction];
+    }
+}
+
+#pragma mark - action
+- (IBAction)showBtnAction:(UIButton *)sender
+{
+    _topLayout.priority = 750;
+    [_tableView setNeedsLayout];
+    [UIView animateWithDuration:0.2 animations:^{
+        [_tableView layoutIfNeeded];
+        _backView.alpha = 1;
+    }];
+}
+
+- (IBAction)hideAction
+{
+    _topLayout.priority = 249;
+    [_tableView setNeedsLayout];
+    [UIView animateWithDuration:0.2 animations:^{
+        [_tableView layoutIfNeeded];
+        _backView.alpha = 0;
+    }];
 }
 
 @end
